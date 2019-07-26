132ea566795268bd356ef4df39fec90e95f3f089
Eagle Code <tomoki.takaba@outlook.jp>
2019-07-27 00:52:40 +0900

    json ファイルに書き込みできるようにしました

--- color_track.py
+++ color_track.py
@@ -21,8 +21,9 @@ while(1):
 
     contours , hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     rects = []
-    
-    file =open('test.pos','w')
+    tar=[0,0,0,0]
+    i = 0
+    file =open('test.json','w')
     for contour in contours:
         approx = cv2.convexHull(contour)
         rect = cv2.boundingRect(approx)
@@ -30,9 +31,12 @@ while(1):
     if len(rects) > 0:
       rect = max(rects, key=(lambda x: x[2] * x[3]))
       cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), (0, 0, 255), thickness=2)
-      print(rect)
-      file.writelines(str(rect))
-      file.close()
+      key = ["x","y","xw","yh"]
+      tar = rect.tolist()
+      key=["x","y","xw","yh"]
+      dic=dict(zip(key,tar))
+      print(dic)
+      json.dump(dic,file)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
